242130e437187e84bccc5bffc03dcc63821d40a9
Filip Mazic <mazza@Filips-Mac.local>
2016-03-06 21:25:51 +0100

    finished pokedex app

---
+++ pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Filip Mazic on 3/6/16.
+//  Copyright © 2016 Filip Mazic. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
